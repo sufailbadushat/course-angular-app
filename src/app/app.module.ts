3a0b2c4bb2a2e655a2f4eae5282126b10fd4a254
sufailbadushat <sufailbadushat@gamil.com>
2022-11-29 12:25:21 +0530

    api services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewCourseComponent } from './view-course/view-course.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const myRoute:Routes=
 [
@@ -36,7 +37,8 @@ const myRoute:Routes=
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
